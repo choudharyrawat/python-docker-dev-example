0349f06d8cf19b2a8e5111144daa3f585486e884
Choudhary Harshit Rawat <choudharyrawat0104@gmail.com>
2025-06-26 16:31:07 +0530

    docker

---
+++ python-test.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "http://localhost:8000/heroes"
+
+data = {
+    "id": 1,
+    "name": "my hero",
+    "secret_name": "austing",
+    "age": 12
+}
+
+response = requests.post(url, json=data)
+print(response.json())
