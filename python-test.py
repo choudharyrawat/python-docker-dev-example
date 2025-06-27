3f916b9f7a09d648da01685be0b7b60c7d9a37be
Choudhary Harshit Rawat <choudharyrawat0104@gmail.com>
2025-06-27 15:32:12 +0530

    Test Check

--- python-test.py
+++ python-test.py
@@ -1,13 +1,8 @@
-import requests
+# import requests
 
-url = "http://localhost:8000/heroes"
+# url = "http://localhost:8000/heroes"
 
-data = {
-    "id": 1,
-    "name": "my hero",
-    "secret_name": "austing",
-    "age": 12
-}
+# data = {"id": 1, "name": "my hero", "secret_name": "austing", "age": 12}
 
-response = requests.post(url, json=data)
-print(response.json())
+# response = requests.post(url, json=data)
+# print(response.json())
